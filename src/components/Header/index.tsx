840ee3c64393bfd7b20862988cc4ee51379e8df4
leomendes18 <leomendesmonteiro1@gmail.com>
2023-10-10 19:19:38 -0300

    update no header e input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import './index.css'
 export function Header(){
   return (
     <div className='header'>
-      <h1 className='title'>Minha lista de Tecnologias</h1>
+      <h1 className="title">Minha lista de Tecnologias</h1>
     </div>
   )
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,14 @@
 import './index.css'
+import simbolo from '../../assets/simbolo.png'
+
 export function Input(){
   return (
     <div className='boxInput'>
       <input type="text" className='input' placeholder='Adicione uma nova tarefa'/>
-      <button>Criar</button>
+      <button className='button'>
+        Criar
+        <img src={simbolo}/>
+      </button>
     </div>
   )
 }
